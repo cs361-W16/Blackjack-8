f7f49b09fd07ecb38996faf8c6189436268914dc
yeja94 <yeja@oregonstate.edu>
2016-03-09 18:17:56 -0800

    Created player, dealer, and incomplete game class
    Hopefully they will pass the Travis.CI

---
+++ src/main/java/models/Game.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Created by Jason Ye on 3/9/2016.
+ */
+public class Game {
+    public ArrayList<Cards> deck;
+    public Player player;
+    public Dealer dealer;
+
+    public Game(){
+        buildDeck();
+        shuffle();
+        player = new Player();
+        dealer = new Dealer();
+        dealTwo();
+    }
+
+    public void buildDeck(){
+        deck = new ArrayList<Cards>();
+        for(int i = 1; i < 14; i++) {
+            deck.add(new Cards(i, Suit.Clubs));
+            deck.add(new Cards(i, Suit.Diamonds));
+            deck.add(new Cards(i, Suit.Hearts));
+            deck.add(new Cards(i, Suit.Spades));
+        }
+    }
+
+    public void shuffle(){
+        long shuffling = System.nanoTime();
+        Collections.shuffle(deck, new Random(shuffling));
+    }
+
+    public void dealTwo() {
+        for (int i = 0; i < 2; i++) {
+            dealer.hit(deck);
+            player.hit(deck);
+        }
+    }
+}
