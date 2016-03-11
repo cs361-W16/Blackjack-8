030167d6edf0d83e53bd634533a229f0dd14c3d2
yeja94 <yeja@oregonstate.edu>
2016-03-10 19:34:28 -0800

    TDD for Cards and Game

---
+++ src/test/java/model/testCards.java
@@ -0,0 +1,23 @@
+package model;
+
+import models.Cards;
+import models.Suit;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Jason Ye on 3/10/2016.
+ */
+public class testCards {
+    @Test
+    public void testSuit(){
+        Cards c = new Cards(7,Suit.Hearts);
+        assertEquals(Suit.Hearts,c.getSuit());
+    }
+
+    @Test
+    public void testString(){
+        Cards c = new Cards(7, Suit.Hearts);
+        assertEquals("7Hearts",c.toString());
+    }
+}
