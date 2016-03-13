a0bc673ab501eddecddf6e91870071ae308e8123
Dioxu <ellis113@gmail.com>
2016-03-13 14:32:32 -0700

    TDD The Card and Player class both have 100% total coverage

--- src/main/java/models/Suit.java
+++ src/main/java/models/Suit.java
@@ -5,5 +5,7 @@ import java.io.Serializable;
  * Created by Jason Ye on 3/8/2016.
  */
 public enum Suit implements Serializable {
-    Hearts, Spades, Diamonds, Clubs
+    Hearts, Spades, Diamonds, Clubs, BadSuit
 }
+
+//Bad suit is used in testing for functions that return type Suit
--- src/test/java/models/testCard.java
+++ src/test/java/models/testCard.java
@@ -1,7 +1,39 @@
 package models;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by Brandon on 3/9/2016.
  */
-public class testCard {
+public class testCard
+{
+    @Test
+    public void testCards()
+    {
+        Cards test = new Cards(2, Suit.Diamonds);
+        assertEquals(Suit.Diamonds, test.suit);
+        assertEquals(2, test.value);
+    }
+
+    @Test
+    public void testGetSuit()
+    {
+        Cards test = new Cards(2, Suit.Diamonds);
+        assertEquals(Suit.Diamonds, test.getSuit());
+    }
+
+    @Test
+    public void testGetValue()
+    {
+        Cards test = new Cards(2, Suit.Diamonds);
+        assertEquals(2, test.getValue());
+    }
+
+    @Test
+    public void testToString()
+    {
+        Cards test = new Cards(2, Suit.Diamonds);
+        assertEquals("2Diamonds", test.toString());
+    }
 }
