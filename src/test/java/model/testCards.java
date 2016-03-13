28095639d89ecbeab0749e50d4ddcc38b126ce41
yeja94 <yeja@oregonstate.edu>
2016-03-12 22:33:48 -0800

    Rewrite TDD for cards class

--- src/test/java/model/testCards.java
+++ src/test/java/model/testCards.java
@@ -6,24 +6,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by Jason Ye on 3/10/2016.
+ * Created by Jason Ye on 3/12/2016.
  */
 public class testCards {
 
     @Test
-    public void testSuit(){
-        Cards c = new Cards(7,Suit.Hearts);
-        assertEquals(Suit.Hearts,c.getSuit());
-    }
-    @Test
-    public void testString(){
-        Cards c = new Cards(7, Suit.Hearts);
-        assertEquals("7Hearts",c.toString());
-    }
-    @Test
-    public void testValue(){
+    public void testToString(){
         Cards c = new Cards(7, Suit.Diamonds);
-        assertEquals(7, c.getValue());
+        assertEquals("7Diamonds", c.toString());
     }
-    
 }
